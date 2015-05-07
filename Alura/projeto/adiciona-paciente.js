470d765b24eeb347ef452538f741b762e21e547e
Wagner Trindade <wtrindades@hotmail.com>
2015-05-07 15:54:12 -0300

    Começei a fazer dentro de adicionar paciente a validação caso o usuário deixe algum campo em branco (Falta arrumar erro quando tenta inserir novo paciente após não ter digitado nada)

--- Alura/projeto/adiciona-paciente.js
+++ Alura/projeto/adiciona-paciente.js
@@ -9,29 +9,39 @@ botao.addEventListener("click", function(event){
 	var campoPeso = document.querySelector("#campo-peso");
 	var campoAltura = document.querySelector("#campo-altura");
 
-	//estrutura de um novo tr com paciente
-	var pacienteNovo = 	"<tr class='paciente'>"+
-						"<td class='info-nome'>"+ campoNome.value +"</td>"+
-						"<td class='info-peso'>"+ campoPeso.value +"</td>"+
-						"<td class='info-altura'>"+ campoAltura.value +"</td>"+
-						"<td class='info-imc'>"+ Math.round(campoPeso.value / (campoAltura.value * campoAltura.value)) +"</td>"+
-					"</tr>"; 
+	//Caso nenhum campo tenha valor vazio
+	if( campoNome.value != "" && campoPeso.value > 0 && campoAltura.value > 0){
+		//estrutura de um novo tr com paciente
+		var pacienteNovo = 	"<tr class='paciente'>"+
+							"<td class='info-nome'>"+ campoNome.value +"</td>"+
+							"<td class='info-peso'>"+ campoPeso.value +"</td>"+
+							"<td class='info-altura'>"+ campoAltura.value +"</td>"+
+							"<td class='info-imc'>"+ Math.round(campoPeso.value / (campoAltura.value * campoAltura.value)) +"</td>"+
+						"</tr>"; 
 
 
-	//document.getElementsByTagName("table")[0]; Chama o elemento por sua tag
+		//document.getElementsByTagName("table")[0]; Chama o elemento por sua tag
 
-	//document.querySelectorAll("table"); Chama o elemento identificando se é tag, classe, ou id e com o All ele monta um array com varios elementos
+		//document.querySelectorAll("table"); Chama o elemento identificando se é tag, classe, ou id e com o All ele monta um array com varios elementos
 
-	var tabela = document.querySelector("table"); //Chama o elemento identificando que é tag e chama apenas 1 elemento sem montar o array
+		var tabela = document.querySelector("table"); //Chama o elemento identificando que é tag e chama apenas 1 elemento sem montar o array
 
-	tabela.innerHTML += pacienteNovo;
+		tabela.innerHTML += pacienteNovo;
+		
+		//Apaga os valores escritos dentro dos campos
+		campoNome.value = "";
+		campoPeso.value = "";
+		campoAltura.value = "";
 
+	}else{
+		var digiteInformacoes = "<p>Erro! É necessário que todos os valores sejam preenchidos para que possa cadastrar um novo paciente.</p>"
 
-	//Apaga os valores escritos dentro dos campos
-	campoNome.value = "";
-	campoPeso.value = "";
-	campoAltura.value = "";
+		var formulario = document.querySelector("form");
 
+		formulario.innerHTML += digiteInformacoes;
+		console.log('Passou aqui');
+
+	}
 
 
 	//event.preventDefault para o padrao de botao dentro de um form que é recarregar a pagina não seja executado
--- Alura/projeto/muda-cor.js
+++ Alura/projeto/muda-cor.js
@@ -3,7 +3,10 @@ var trs = document.getElementsByTagName("tr");
 percorreArray(trs, function(tr){
 
 	tr.addEventListener("mouseover", function(){
-		this.setAttribute("bgcolor", "#ddd");
+		this.setAttribute("bgcolor", "#eee");
+	});
+	tr.addEventListener("mouseout", function(){
+		this.setAttribute("bgcolor", "#fff");
 	});
 
 }); 
